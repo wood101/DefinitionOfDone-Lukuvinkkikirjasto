91cea8ba5809e3c7053a9fb201dff88c8f5712e5
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-11-30 11:04:20 +0200

    cucumberia säädetty

--- src/test/java/ReadMe/Stepdefs.java
+++ src/test/java/ReadMe/Stepdefs.java
@@ -1,29 +1,15 @@
-package ReadMe.ui;
+package ReadMe;
 
-import ReadMe.domain.Bookmark;
 import ReadMe.ui.UI;
 import ReadMe.io.IOStub;
 import ReadMe.dao.InMemoryDao;
-import ReadMe.dao.VideoDao;
-import ReadMe.database.Database;
-import ReadMe.database.SQLiteDatabase;
 import ReadMe.domain.Blog;
 import ReadMe.domain.Book;
 import ReadMe.domain.News;
-import ReadMe.domain.Video;
-import ReadMe.io.ConsoleIO;
-import ReadMe.io.IO;
-import cucumber.api.PendingException;
-import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.Assert.*;
-import org.junit.Before;
-import static org.mockito.Mockito.mock;
 
 public class Stepdefs {
 
@@ -40,19 +26,6 @@ public class Stepdefs {
     String[] inputLinesArticle = new String[10];
     String[] inputLinesBlog = new String[10];
 
-//    @Given("^command new tip is selected$")
-//    public void command_new_tip_is_selected() throws Throwable {
-//        inputLinesAdd[0] = "a";
-//
-//    }
-//    @When("^title \"([^\"]*)\" and description \"([^\"]*)\" and link \"([^\"]*)\" are given$")
-//    public void title_and_description_and_link_are_given(String title, String description, String link) throws Throwable {
-//        inputLinesAdd[1] = "title";
-//        inputLinesAdd[2] = "desc";
-//        inputLinesAdd[3] = "www";
-//        inputLinesAdd[4] = "q";
-//
-//    }
     // list all
     @When("^command list all is given$")
     public void command_list_all_is_given() throws Throwable {
@@ -60,24 +33,6 @@ public class Stepdefs {
         inputLinesAll[1] = "q";
     }
 
-//    @Then("^a new tip is added$")
-//    public void a_new_tip_is_added() throws Throwable {
-//
-//        io = new IOStub(inputLinesAdd);
-//        UI ui = new UI(io, dao);
-//        ui.run();
-//        assertTrue(dao.listAll().contains(new Bookmark("title", "desc", "www")));
-//    }
-//    @Then("^all bookmarks are printed$")
-//    public void all_bookmarks_are_printed() throws Throwable {
-//        IOStub ios = new IOStub(inputLinesList);
-//        UI ui = new UI(ios, dao);
-//        ui.run();
-//        assertTrue(ios.getOutputs().contains(new Bookmark("Kakkakuutiot", "Tämä on tärkeää tietoa",
-//                "https://www.iltalehti.fi/terveysuutiset/a/71cb5062-ed6e-45a6-a22c-d0949daeaa6a").toString())
-//                && ios.getOutputs().contains(new Bookmark("Täydelliset joulutortut", "Näin teet täydellisen tortun",
-//                        "https://www.iltalehti.fi/ruoka-artikkelit/a/556f9c4c-638b-4091-ab35-cb6e52e02555").toString()));
-//    }
     //for video
     @Given("^command \"([^\"]*)\" add new readtip is selected and command \"([^\"]*)\" video is selected$")
     public void command_add_new_readtip_is_selected_and_command_video_is_selected(String arg1, String arg2) throws Throwable {
