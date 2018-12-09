4a60dfeabb18069dc667f351e8c6f0c3563d05b3
Jaakko Koli <jaakko.koli@gmail.com>
2018-12-09 23:16:12 +0200

    refraktorointia

--- src/main/java/ReadMe/io/IO.java
+++ src/main/java/ReadMe/io/IO.java
@@ -1,8 +1,5 @@
 package ReadMe.io;
 
-import ReadMe.domain.ReadingTip;
-import java.util.List;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
